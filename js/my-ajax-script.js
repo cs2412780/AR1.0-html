2165b075a3026fb53d9aaaa9284e57c3aee0a22d
Liang Dong <liangdong@cpp.edu>
2018-02-03 16:09:28 -0800

    AR 1.0

---
+++ js/my-ajax-script.js
@@ -0,0 +1,10 @@
+jQuery.ajax(
+    {
+        type: "post",
+        dataType: "json",
+        url: my_ajax_object.ajax_url,
+        data: formData,
+        success: function(msg){
+            console.log(msg);
+        }
+    });
